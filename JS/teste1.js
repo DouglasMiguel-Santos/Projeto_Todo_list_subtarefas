676ffccc01a09b5c8a078ff883872d1240cf1c19
DouglasMiguel-Santos <douglasmb.santos@gmail.com>
2024-10-25 19:19:32 -0300

    correção de erros para criar novas subtarefas

--- JS/teste1.js
+++ JS/teste1.js
@@ -83,7 +83,7 @@ function adicionarItem() {
             />
           </svg>
  </li>
-         <div id="container-subtarefas" class="hidden subtarefas  subtarefas-${posicao} "
+         <div id="container-subtarefas" class="hidden subtarefas  subtarefas-${posicao} ">
           <ul class="subtarefas  ">
             
             <li>
@@ -166,6 +166,58 @@ function criarSubtarefa(posicao) {
 
   localStorage.setItem("lista", JSON.stringify(Lista));
 }
+// Função para criar uma nova subtarefa
+function criarSubtarefa(posicao) {
+  // Seleciona o input de subtarefas e o contêiner de subtarefas para a posição fornecida
+  let inputSubtarefas = document.querySelector(
+    `.subtarefas-${posicao} .input-subtarefas`
+  );
+  let subtarefaContainer = document.querySelector(`.subtarefas-${posicao}`);
+  let novaSubtarefaTitulo = inputSubtarefas.value;
+
+  if (novaSubtarefaTitulo.trim() !== "") {
+    // Adiciona a nova subtarefa ao array de subtarefas da tarefa específica
+    Lista[posicao].subtarefas.push({
+      titulo: novaSubtarefaTitulo,
+      concluida: false,
+    });
+
+    // Atualiza o HTML para mostrar a nova subtarefa na lista de subtarefas
+    subtarefaContainer.innerHTML += `
+      <li id="subLi" class="subtarefas-${posicao}">
+        <input type="checkbox" class="sub-check" />
+        <p>${novaSubtarefaTitulo}</p>
+        <svg
+          style="width: 20px"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="btn-deletarSubtarefa"
+          id="btn-deletarSubtarefa"
+          onclick="deletarSubtarefa(${posicao}, ${
+      Lista[posicao].subtarefas.length - 1
+    })"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+          />
+        </svg>
+      </li>
+    `;
+
+    // Limpa o campo de input de subtarefas
+    inputSubtarefas.value = "";
+
+    // Salva a lista atualizada no localStorage
+    localStorage.setItem("lista", JSON.stringify(Lista));
+  } else {
+    alert("Por favor, escreva uma subtarefa.");
+  }
+}
 
 //DELETAR UM ITEM DA LISTA
 function deletarTarefa(posicao) {
@@ -200,12 +252,12 @@ function esconderSubtarefa(posicao) {
 
 recarregartarefas();
 
-let lista = [
-  {
-    tarefa: "Estudar JavaScript",
-    subtarefas: [
-      { titulo: "Revisar arrays", concluida: false },
-      { titulo: "Praticar loops", concluida: true },
-    ],
-  },
-];
+// let lista = [
+//   {
+//     tarefa: "Estudar JavaScript",
+//     subtarefas: [
+//       { titulo: "Revisar arrays", concluida: false },
+//       { titulo: "Praticar loops", concluida: true },
+//     ],
+//   },
+// ];
